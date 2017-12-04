362007a7543378708f3152d3e90c12dcc6453ba4
dmasieh <dmasieh@gmail.com>
2017-12-04 16:51:48 -0500

    12/4/17 fixed carousel mistake

--- js/init.js
+++ js/init.js
@@ -3,5 +3,10 @@ $( document ).ready(function() {
     $('.collapsible').collapsible('open', 0);
     $('#closecollap').collapsible('close', 0);
     $('.carousel').carousel();
+    autoplay()   
+	function autoplay() {
+    	$('.carousel').carousel('next');
+    	setTimeout(autoplay, 5500);
+	}
     $('.tooltipped').tooltip({delay: 50});
 });
