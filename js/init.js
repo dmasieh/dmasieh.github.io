02778976a087411ca52f2bbeaa1e88c9dc89366e
dmasieh <dmasieh@gmail.com>
2017-12-04 16:46:13 -0500

    12/4/17 4:41pm

--- js/init.js
+++ js/init.js
@@ -2,6 +2,6 @@ $( document ).ready(function() {
     $(".button-collapse").sideNav();
     $('.collapsible').collapsible('open', 0);
     $('#closecollap').collapsible('close', 0);
-    $('.carousel').carousel({fullWidth: true});
+    $('.carousel').carousel();
     $('.tooltipped').tooltip({delay: 50});
 });
